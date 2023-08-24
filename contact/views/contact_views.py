c7a8715d62bc7decbda6e704396b389a90a91579
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2023-08-24 00:11:44 -0300

    add search bar

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 
@@ -7,6 +7,33 @@ from contact.models import Contact
 def index(request):
     contacts = Contact.objects \
         .filter(show=True) \
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context=context,
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if not search_value:
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .filter(
+            Q(first_name__icontains=search_value) |  # noqa: W504
+            Q(last_name__icontains=search_value) |  # noqa: W504
+            Q(phone__icontains=search_value) |  # noqa: W504
+            Q(email__icontains=search_value)
+        ) \
         .order_by('-id')
 
     context = {
