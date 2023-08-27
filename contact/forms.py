2a61ea2b57720277f5ba3beacab2099c93c81911
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2023-08-27 00:33:29 -0300

    create user form

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 from . import models
 
@@ -37,3 +39,36 @@ class ContactForm(forms.ModelForm):
             )
 
         return super().clean()
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+    )
+
+    last_name = forms.CharField(
+        required=True,
+    )
+
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError(
+                    'Email already exists',
+                    code='invalid'
+                )
+            )
+
+        return email
