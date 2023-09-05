71d89b6c583bfbaba0b412ad867b06a1caa594d8
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2023-09-04 22:03:30 -0300

    improve login system

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 
 from contact.forms import ContactForm
 from contact.models import Contact
 
 
 # Create your views here.
+@login_required(login_url='contact:login')
 def create(request):
     heading = 'Create '
     site_title = f'{heading}- '
@@ -23,7 +26,9 @@ def create(request):
         }
 
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             return redirect('contact:update', contact_id=contact.pk)
 
         return render(
@@ -46,13 +51,16 @@ def create(request):
     )
 
 
+@login_required(login_url='contact:login')
 def update(request, contact_id):
     heading = 'Update '
     site_title = f'{heading}- '
 
     form_action = reverse('contact:update', args=(contact_id,))
 
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True, owner=request.user,
+    )
 
     if request.method == 'POST':
         form = ContactForm(request.POST, request.FILES, instance=contact)
@@ -88,8 +96,11 @@ def update(request, contact_id):
     )
 
 
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True, owner=request.user,
+    )
 
     confirmation = request.POST.get('confirmation', 'no')
     if confirmation == 'yes':
